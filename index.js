5f46f2230bc8791ac208fb3df10fd545f33e1e5b
Saikat Das <dassaikat93@yahoo.com>
2018-03-27 23:47:00 +0530

    edited display message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World to our Reminder App!'))
+app.get('/', (req, res) => res.send('Hello World to our Reminder App! We are super excited to start our journey together'))
 
 app.listen(3000, () => console.log('ReminderMe app listening on port 3000!'))
