01653d72d227a01ab3aa28a57d4b764a5367785b
Saikat Das <dassaikat93@yahoo.com>
2018-04-02 01:19:52 +0530

    changed port to 8080 and display message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World to our Reminder App! We are super excited to start our journey together OMG'))
+app.get('/', (req, res) => res.send('Hello World to our Reminder App! We are super excited to start our journey together OMG.How are you doing today?'))
 
 app.listen(8080, () => console.log('ReminderMe app listening on port 8080!'))
